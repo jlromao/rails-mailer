b5f8c0d20523cbf0a2b3b0d2de8484715d250936
Jorge Romao <jorgeromao83@gmail.com>
2018-01-30 11:17:28 -0500

    created email controller and view

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+  default from: "jorgeromao83@gmail.com"
+  puts "UserMailer initialize"
+
+  def welcome_email(user)
+    p "welcome_email action fired"
+    @user = user
+    mail(to: @user.email, subject:"Welcome!")
+  end
+end
